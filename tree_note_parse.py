adcfdb23a66778c7bafba2ddf87aaf2d9b369795
Jesse Aldridge <JesseAldridge@gmail.com>
2016-07-04 20:33:02 -0700

    initial

---
+++ tree_note_parse.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+with open 'input.txt' as f:
+  text = f.read()
+
+
+class Node:
+  def __init__(self, line):
+    self.line = line
+    self.children = []
+    self.indiv_hours, self.cum_hours = 0, 0
+    self.value = None
+
+    # Parse out existing metadata in either full or shorthand formats.
+
+    def full_meta():
+      self.value, self.indiv_hours, self.cum_hours, _ = [
+        float(re.sub('[^0-9\.]', '', s)) for s in match.group(1).split()]
+
+    def short_meta():
+      self.value, self.indiv_hours = [
+        float(re.sub('[^0-9\.]', '', s)) for s in match.group(1).split()]
+
+    def simple_hours():
+      self.value = 0
+      self.indiv_hours = float(match.group(1))
+      self.cum_hours = self.indiv_hours
+
+    for meta_regex, parse in (
+      ('([0-9\.]+v [0-9\.]+/[0-9\.]+h \([0-9\.]+\))$', full_meta),
+      ('([0-9\.]+v [0-9\.]+h)$', short_meta),
+      ('([0-9\.]+)$', simple_hours)):
+      match = re.search(meta_regex, line)
+      if match:
+        parse(match, line)
+        self.line = re.sub(meta_regex, '', line)
+        break
+
+  def calc_meta(self):
+    " cumulative hours = individual hours + cumulative hours of children "
+
+    cum_hours = self.indiv_hours
+    for child in self.children:
+      child.calc_meta()
+      if child.value is not None:
+        cum_hours += child.cum_hours
+    self.cum_hours = cum_hours
+    self.value_per_hour = 0
+    if self.value is not None:
+      if self.cum_hours == 0:
+        self.value_per_hour = self.value
+      else:
+        self.value_per_hour = self.value / self.cum_hours
+    self.children.sort(key=lambda node: node.value_per_hour)
+
+  def render_and_print(self):
+    " Include metadata with line. "
+
+    full_line = '{} {}v {}/{}h ({0:.2f})'.format(
+      self.line, self.value, self.indiv_hours, self.cum_hours)
+    print full_line
+    for child in self.children:
+      child.render_and_print()
+
+
+def build_tree(root_line, line_iter):
+  ' Convert indented lines of text to Nodes. '
+
+  root = Node(root_line)
+  try:
+    next_line = line_iter.next()
+  except StopIteration:
+    return root, None
+  while next_line is not None and indent(next_line) > indent(root_line):
+    child, next_line = build_tree(next_line, line_iter)
+    root.children.append(child)
+  return root, next_line
+
+def indent(line):
+  return (len(line) - len(line.lstrip())) / 2
+
+
+def main():
+  ' Convert text into Nodes.  Store top level nodes.  Process tree. '
+
+  line_iter = iter(line.rstrip() for line in text.splitlines() if line.rstrip())
+  top_nodes = []
+  next_line = line_iter.next()
+  while next_line is not None:
+    child, next_line = build_tree(next_line, line_iter)
+    top_nodes.append(child)
+  for node in top_nodes:
+    calc_meta(node)
+
+  for node in top_nodes:
+    node.render_and_print()
